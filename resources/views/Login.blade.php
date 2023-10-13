f0968a3b6e0329f5e585cee007ee769277be988f
bakasibruceto <abla09027@gmail.com>
2023-10-13 22:30:05 +0800

    login system

---
+++ resources/views/Login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet"> -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
+
+</head>
+
+<body>
+    <main class="container">
+        <div class="grid">
+            <h1>Login</h1>
+            <form action="../include/login.inc.php" method="POST">
+                <label for="username">username or email
+                    <input type="text" name="username">
+                </label>
+                <label for="password">password
+                    <input type="password" name="password">
+                </label>
+                <button type="submit" name="submit">Login</button>
+                <button>Forgot password</button>
+            </form>
+           
+        </div>
+    </main>
+
+</body>
+
+</html>
---
+++ resources/views/Signup.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link href=" https://cdn.jsdelivr.net/npm/@picocss/pico@1.5.10/css/pico.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/scss/app.scss'])
+</head>
+
+<body>
+    <main class="container">
+        <form action="../include/Signup.inc.php" method="POST">
+            <div class="grid">
+                <h1>Sign up</h1>
+                <div>
+                    <label for="username">username
+                        <input type="text" name="username">
+                    </label>
+                    <label for="email">email
+                        <input type="text" name="email">
+                    </label>
+                    <label for="password">password
+                        <input type="password" name="password">
+                    </label>
+                    <label for="password">Re-Enter password
+                        <input type="password" name="repassword">
+                    </label>
+                    <button type="submit" name="submit">sign up </button>
+                </div>
+            
+            </div>
+        
+        </form>
+    </main>
+
+</body>
+
+</html>
