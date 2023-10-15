e86d2874abf10ceb668b4d922f062641015fd69b
bakasibruceto <abla09027@gmail.com>
2023-10-15 12:31:34 +0800

    Customized email template

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Notifications\NewEmailVerificationNotification;
+use App\Notifications\NewResetPasswordNotification;
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function sendPasswordResetNotification($token){
+        $this->notify(new NewResetPasswordNotification($token));
+    }
+
+    public function sendEmailVerificationNotification(){
+        $this->notify(new NewEmailVerificationNotification);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -43,4 +55,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    
 }
