9073e163cde23db783fe1bf576e8de0a4ab91702
bakasibruceto <abla09027@gmail.com>
2023-10-15 12:32:08 +0800

    change expiration from 60 to 5 mins

--- config/auth.php
+++ config/auth.php
@@ -94,8 +94,8 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_reset_tokens',
-            'expire' => 60,
-            'throttle' => 60,
+            'expire' => 5,
+            'throttle' => 5,
         ],
     ],
 
