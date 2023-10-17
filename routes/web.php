8e12af8d9ed70578ebcda4be57e6207ed1980080
bakasibruceto <abla09027@gmail.com>
2023-10-17 12:50:49 +0800

    new routes added

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return view('dashboard');
     })->name('dashboard');
 
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/home', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    
+       
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+   
+    Route::get('/myprofile', function () {
+        return view('profile.show');
+    })->name('myprofile');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
